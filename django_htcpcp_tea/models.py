dc9ceb56a3b8b9aad37f4dfaf9e33b261be968f6
Brian Schubert <brianm.schubert@gmail.com>
2019-06-17 20:39:06 -0400

    Add initial models

---
+++ django_htcpcp_tea/models.py
@@ -0,0 +1,58 @@
+#  Copyright (c) 2019 Brian Schubert
+#
+#  This file is distributed under the MIT License. If a copy of the
+#  MIT License was not distributed with this file, you can obtain one
+#  at https://opensource.org/licenses/MIT.
+
+from django.core.exceptions import ValidationError
+from django.db import models
+from django.utils.functional import cached_property
+
+
+class Pot(models.Model):
+    """A Tea- or Coffee Pot capable of brewing a choice beverage."""
+    name = models.CharField(max_length=35, unique=True)
+
+    brew_coffee = models.BooleanField(verbose_name='Able to brew coffee?', default=True)
+
+    supported_teas = models.ManyToManyField('TeaType')
+
+    supported_additions = models.ManyToManyField('Addition')
+
+    @cached_property
+    def tea_capable(self):
+        return self.supported_teas.exists()
+
+    @property
+    def is_teapot(self):
+        return self.tea_capable and not self.brew_coffee
+
+
+class TeaType(models.Model):
+    """
+    A variety of tea that can be brewed by a pot.
+
+    Per the HTCPCP standard, tea may be available as tea bags or tea leaves.
+    """
+    name = models.CharField(verbose_name='Tea name', max_length=35, unique=True)
+
+    slug = models.SlugField(unique=True)
+
+
+class Addition(models.Model):
+    """
+    A beverage addition that may be specified in the Accept-Additions header
+    field of an HTCPCP request.
+    """
+    name = models.CharField(max_length=35, unique=True)
+
+    def clean(self):
+        # Calling parent in case it is given a non-empty body in the
+        # future (currently it is empty).
+        super().clean()
+
+        error_msg = ("Decaffeinated addition not allowed. RFC 2324 specifies "
+                     "no option for decaffeinated coffee. What's the point? ")
+
+        if 'decaffeinated' in self.name.lower():
+            raise ValidationError(error_msg)
