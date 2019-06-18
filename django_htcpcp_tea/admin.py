b1c3f6b2067d75e71a1e7b0e227df8501b013f13
Brian Schubert <brianm.schubert@gmail.com>
2019-06-18 13:41:45 -0400

    Add initial app admin

---
+++ django_htcpcp_tea/admin.py
@@ -0,0 +1,52 @@
+#  Copyright (c) 2019 Brian Schubert
+#
+#  This file is distributed under the MIT License. If a copy of the
+#  MIT License was not distributed with this file, you can obtain one
+#  at https://opensource.org/licenses/MIT.
+
+from django.contrib import admin
+
+from .models import Addition, Pot, TeaType
+
+
+@admin.register(Pot)
+class PotAdmin(admin.ModelAdmin):
+    search_fields = ('supported_teas__name', 'supported_additions__name')
+
+    fields = (('name', 'brew_coffee'), 'supported_teas', 'supported_additions')
+
+    list_display = ('id', 'name', 'brew_coffee', 'is_teapot_view')
+
+    list_display_links = ('id', 'name')
+
+    filter_horizontal = ('supported_teas', 'supported_additions')
+
+    list_filter = ('brew_coffee', ('supported_teas', admin.RelatedOnlyFieldListFilter))
+
+    save_as = True
+
+    def is_teapot_view(self, obj):
+        return obj.is_teapot
+
+    is_teapot_view.boolean = True
+    is_teapot_view.short_description = "Is a teapot?"
+
+
+@admin.register(TeaType)
+class PotAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+    fieldsets = (
+        (None, {
+            'fields': ('name',)
+        }),
+        ('Advanced', {
+            'classes': ('collapse',),
+            'fields': ('slug',),
+        }),
+    )
+
+
+@admin.register(Addition)
+class PotAdmin(admin.ModelAdmin):
+    pass
