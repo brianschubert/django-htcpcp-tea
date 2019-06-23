a10f6a691cae428a3c23a760b70a43e743ff1e62
Brian Schubert <brianm.schubert@gmail.com>
2019-06-23 11:30:06 -0400

    Bump version to v0.1.1

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ CLASSIFIERS = [
 
 setup(
     name='django-htcpcp-tea',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     license='MIT',
     author='Brian Schubert',
