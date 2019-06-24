e9180a69e272874c2159df9b24fe9ae7d8207381
Brian Schubert <brianm.schubert@gmail.com>
2019-06-24 15:55:06 -0400

    Bump version to v0.3.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Brian Schubert'
 author = 'Brian Schubert'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.2'
+release = '0.3.0'
 
 
 # -- General configuration ---------------------------------------------------
